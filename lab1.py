f1cd481aed350fd8c3802c448a84bd7c66ec352c
KODER-STUDENT <yuriykhomynmi@gmail.com>
2024-09-17 11:46:03 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,23 @@
+drink = "water"
+print(drink)
+
+
+
+is_holiday = True
+is_workday = False
+print(is_holiday)
+
+
+
+is_sunny = True
+is_cloudy = True
+#the weather is_sunny or the weather is_cloudy
+
+
+import math
+x=2.34
+y=1.45
+result = math.e**-y+(math.sin(2*x))/(math.cos (y**2))-12.1*math.log(x+y)
+print(result)
+ 
+
